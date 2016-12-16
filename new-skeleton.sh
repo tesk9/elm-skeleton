b8a5254e3571a889da2256ed7f892877f86b33d4
Tessa Kelly <tessakkelly@gmail.com>
2016-12-16 12:09:38 -0800

    Begin writing user interface

--- new-skeleton.sh
+++ new-skeleton.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo hello world
+pathString="$1"
+namespace=${pathString////-}
+
+echo Creating a new elm skeleton
+echo Path: $pathString
+echo Namespace: "$namespace"
+
+echo "Include a new elm-package.json?"
+select opt in "Yes" "No"; do
+   if [ "$opt" = "Yes" ]; then
+    echo TODO
+    exit
+   elif [ "$opt" = "No" ]; then
+    echo TODO
+    exit
+   else
+    echo please enter 1 or 2
+   fi
+done
