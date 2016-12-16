a6bfde698743a461d44802ab6c0a41418410eaab
Tessa Kelly <tessakkelly@gmail.com>
2016-12-16 14:19:20 -0800

    Move the elm package json conditionally

--- new-skeleton.sh
+++ new-skeleton.sh
@@ -4,15 +4,35 @@ path="$1"
 namespace=${path////-}
 
 if [ ! -d "$path" ]; then
-    echo $path not found. Operation cancelled.
+    echo Path $path not found. Operation cancelled.
     exit
 fi
 
 duplicateSkeleton ()
 {
-    cp ./skeleton/* $path
+    cp ./skeleton/* $1
 }
 
+duplicateElmPackage ()
+{
+    cp elm-package.json $1
+}
+
+createNewProject ()
+{
+    srcPath=$1/src
+    if [ ! -d "$srcPath" ]; then
+        mkdir $srcPath
+    fi
+    duplicateSkeleton $srcPath
+    duplicateElmPackage $1
+
+}
+
+addFeature ()
+{
+    duplicateSkeleton $1
+}
 
 echo Creating a new elm skeleton
 echo Path: $path
@@ -21,11 +41,10 @@ echo Namespace: "$namespace"
 echo "Include a new elm-package.json?"
 select opt in "Yes" "No"; do
    if [ "$opt" = "Yes" ]; then
-    duplicateSkeleton
-    echo TODO
+    createNewProject $path
     exit
    elif [ "$opt" = "No" ]; then
-    duplicateSkeleton
+    addFeature $path
     exit
    else
     echo please enter 1 or 2
