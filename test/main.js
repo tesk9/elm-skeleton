3121a0b7ce6138425f8c57ab1ceb370088b84fca
Tessa Kelly <tessakkelly@gmail.com>
2017-03-12 23:39:43 -0700

    Adds empty test file

---
+++ test/main.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('New Feature', function() {
+  xit('should work', function() {});
+});
+
+describe('New Project', function() {
+  xit('should work', function() {});
+});
