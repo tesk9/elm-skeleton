e952dee09d3c97ee609ae4a2e9aa5eb75beb5e50
Tessa Kelly <tessakkelly@gmail.com>
2017-07-02 13:10:56 -0700

    Use the module skeleton for new features

--- scripts/newFeature.js
+++ scripts/newFeature.js
@@ -3,21 +3,21 @@ var fs = require("fs-extra");
 var moveProject = require("elm-move/scripts/project");
 var helpers = require("./helpers");
 
-var skeletonPath = path.join(__dirname, "../skeleton");
-var tempDirectory = path.join(__dirname, "../SkeletonTemp");
+var moduleSkeletonPath = path.join(__dirname, "../module");
+var tempDirectory = path.join(__dirname, "../ModuleTemp");
 
 function createNewFeature(destination) {
   console.log("Creating a new project feature at", destination);
 
   // Set up temporary processing directory
-  var tempDirectory = path.join(__dirname, "../skeleton_temp");
+  var tempDirectory = path.join(__dirname, "../module_temp");
   helpers.addFolder(tempDirectory);
 
   // Ensure destination exists
   helpers.addFolder(destination);
 
   // Copy the module to the temporary processing directory
-  fs.copy(skeletonPath, tempDirectory, function(err) {
+  fs.copy(moduleSkeletonPath, tempDirectory, function(err) {
     if (err) { throw err; }
     console.log("Empty feature module copied to temporary directory for processing.");
 
