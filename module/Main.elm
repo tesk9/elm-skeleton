aad0eee13a95d70cffbad22a350d7373546e8932
Tessa Kelly <tessakkelly@gmail.com>
2017-07-08 16:09:26 -0700

    Expose update from Main in module

--- module/Main.elm
+++ module/Main.elm
@@ -1,15 +1,17 @@
 module Main
     exposing
         ( Flags
-        , init
-        , decoder
         , Model
         , Msg
-        , view
+        , decoder
+        , init
         , styles
+        , update
+        , view
         )
 
 {-|
+
 @docs Flags, init
 
 @docs Model, model
@@ -19,66 +21,59 @@ module Main
 @docs view
 
 @docs styles
+
 -}
 
-import Html exposing (Html)
-import Json.Decode
 import Flags as Flags
+import Html exposing (Html)
 import Init as Init
+import Json.Decode
 import Model as Model
+import Styles as Styles
 import Update as Update
 import View as View
-import Styles as Styles
 
 
-{-|
--}
+{-| -}
 type alias Flags =
     Flags.Flags
 
 
-{-|
--}
+{-| -}
 decoder : Json.Decode.Decoder Flags.Flags
 decoder =
     Flags.decoder
 
 
-{-|
--}
+{-| -}
 init : Flags -> Model
 init =
     Init.init
 
 
-{-|
--}
+{-| -}
 type alias Model =
     Model.Model
 
 
-{-|
--}
+{-| -}
 type alias Msg =
     Update.Msg
 
 
-{-|
--}
+{-| -}
 update : Msg -> Model -> ( Model, Cmd Msg )
 update =
     Update.update
 
 
-{-|
--}
+{-| -}
 view : Model -> Html Msg
 view =
     View.view
 
 
-{-|
--}
+{-| -}
 styles : String
 styles =
     Styles.styles
